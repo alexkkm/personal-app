47ad3a5380299d7e7667613f69882e7a9a61dae1
alexkkm <alexkong0222@gmail.com>
2024-04-18 17:45:08 +0800

    refine the details of messageBoard

--- pages/CyberpunkPage.js
+++ pages/CyberpunkPage.js
@@ -1,25 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
 import MessageBoard from './MessageBoard';
 
 const CyberpunkPage = ({ route, navigation }) => {
     const { itemId = "", otherField = "" } = route.params || {};
 
     return (
-        <View style={styles.main}>
-            <Text style={styles.text}>Cyberpunk Page</Text>
-            <StatusBar style="hidden" />
-            <View style={styles.box}></View>
-            <MessageBoard />
-        </View >
+        <ScrollView style={[]}>
+            <View style={styles.main}>
+                <Text style={styles.text}>Cyberpunk Page</Text>
+                <StatusBar style="hidden" />
+                <View style={styles.box}></View>
+                <MessageBoard />
+            </View >
+        </ScrollView>
+
     );
 }
 
 const styles = StyleSheet.create({
     main: {
         backgroundColor: '#000000',
-        width: 1000,
-        height: 1000,
+        width: '100%',
+        height: 1000
     },
     text: {
         color: '#00f0ff'
--- pages/MessageBoard.js
+++ pages/MessageBoard.js
@@ -4,8 +4,8 @@ import { View, Text, StyleSheet } from 'react-native';
 const MessageBoard = () => {
     return (
         <View style={styles.messageBoard}>
-            <View style={styles.pad}>
-                <View style={styles.padBody}>
+            <View style={styles.border}>
+                <View style={styles.body}>
                     <Text style={styles.heading}>What's this?</Text>
                     <Text style={styles.paragraph}>
                         A fake Slack or Discord type of app inspired by Cyberpunk 2077. This app is static, eg. not implementing much logic.
@@ -21,12 +21,17 @@ const MessageBoard = () => {
 
 const styles = StyleSheet.create({
     messageBoard: {
-        left: 10,
-        top: 200,
-        width: 400,
+        // position
+        left: '0%',
+        top: "5%",
+        // size
+        width: '100%',
     },
-    pad: {
+    border: {
+        // background color
         backgroundColor: 'rgba(0, 240, 255, 0.05)',
+
+        // border
         borderWidth: 1,
         borderColor: '#00f0ff',
         borderRadius: 5,
@@ -34,16 +39,19 @@ const styles = StyleSheet.create({
         //TODO: clipPath is not supported by react-native, finding alernatives
         //clipPath: 'polygon(0 0, 100% 0, 100% calc(100% - 1rem + 2px), calc(100% - 1rem + 2px) 100%, 0 100%)',
     },
-    padBody: {
+    body: {
+        // padding
         padding: 12,
         paddingBottom: 16,
     },
+    // Text heading 
     heading: {
         color: '#2be4ea',
         fontFamily: 'Rajdhani',
         fontSize: 24,
         lineHeight: 22.4,
     },
+    // Text paragraph
     paragraph: {
         color: '#2be4ea',
         fontFamily: 'Rajdhani',
