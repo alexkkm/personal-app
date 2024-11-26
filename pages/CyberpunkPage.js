88cd70453f9a9382985310d7ae073a9834a1aa34
Alexkkm <alexkong0222@gmail.com>
2024-11-26 03:57:51 +0000

    moved all widgets into /widgets

--- pages/CyberpunkPage.js
+++ pages/CyberpunkPage.js
@@ -1,64 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
-import MessageBoard from './MessageBoard';
-import DarkShadowButton from './DarkShadowButton';
-import LightBorderButton from './LightBorderButton';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+} from "react-native";
+import MessageBoard from "../widgets/MessageBoard";
+import DarkShadowButton from "../widgets/DarkShadowButton";
+import LightBorderButton from "../widgets/LightBorderButton";
 
 const CyberpunkPage = () => {
-    return (
-        <ScrollView style={[]}>
-            <View style={styles.main}>
-                <Text style={styles.text}>Cyberpunk Page</Text>
-                <StatusBar style="hidden" />
-                {/*<View style={styles.box}></View>*/}
-                <MessageBoard title="Example Heading" textList={["Example of paragraph.", "Another Example of Paragraph, which may occupy for two lines."]} />
-                <View style={{
-                    // size
-                    width: 100,
-                    // position
-                    left: '10%',
-                    top: "10%",
-                }}>
-                    <DarkShadowButton buttonTitle="Button with dark shadow" buttonFunction={() => { console.log("function of the button triggered") }} />
-                    <LightBorderButton buttonTitle="Button with Light Border" buttonFunction={() => { console.log("function of the button triggered") }} />
-                </View>
-
-            </View >
-        </ScrollView>
-
-    );
-}
+  return (
+    <ScrollView style={[]}>
+      <View style={styles.main}>
+        <Text style={styles.text}>Cyberpunk Page</Text>
+        <StatusBar style="hidden" />
+        {/*<View style={styles.box}></View>*/}
+        <MessageBoard
+          title="Example Heading"
+          textList={[
+            "Example of paragraph.",
+            "Another Example of Paragraph, which may occupy for two lines.",
+          ]}
+        />
+        <View
+          style={{
+            // size
+            width: 100,
+            // position
+            left: "10%",
+            top: "10%",
+          }}
+        >
+          <DarkShadowButton
+            buttonTitle="Button with dark shadow"
+            buttonFunction={() => {
+              console.log("function of the button triggered");
+            }}
+          />
+          <LightBorderButton
+            buttonTitle="Button with Light Border"
+            buttonFunction={() => {
+              console.log("function of the button triggered");
+            }}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-    main: {
-        backgroundColor: '#000000',
-        width: '100%',
-        height: 1000
-    },
-    text: {
-        color: '#00f0ff'
-    },
-    box: {
+  main: {
+    backgroundColor: "#000000",
+    width: "100%",
+    height: 1000,
+  },
+  text: {
+    color: "#00f0ff",
+  },
+  box: {
+    // Size
+    width: 100,
+    height: 100,
 
-        // Size
-        width: 100,
-        height: 100,
+    // inner
+    backgroundColor: "rgba(0, 240, 255, 0.05)",
+    padding: 10,
 
-        // inner
-        backgroundColor: 'rgba(0, 240, 255, 0.05)',
-        padding: 10,
+    // border
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#00f0ff",
 
-        // border
-        borderRadius: 10,
-        borderWidth: 2,
-        borderColor: '#00f0ff',
-
-        // shadow
-        shadowOffset: { height: 1, width: 1 },
-        shadowColor: '#00f0ff',
-        shadowRadius: 0.1,
-        shadowOpacity: 0.5,
-    },
+    // shadow
+    shadowOffset: { height: 1, width: 1 },
+    shadowColor: "#00f0ff",
+    shadowRadius: 0.1,
+    shadowOpacity: 0.5,
+  },
 });
 
 export default CyberpunkPage;
--- pages/DesignPage.js
+++ pages/DesignPage.js
@@ -1,74 +1,86 @@
-import { Button, StyleSheet, Text, TextInput, View, ScrollView, TouchableOpacity, } from 'react-native';
-import WeatherWidget from './WeatherBox';
-import MessageBoard from './MessageBoard';
-import ClockWidget from './Clock';
-import { useNavigation } from '@react-navigation/native';
-
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import WeatherWidget from "../widgets/WeatherBox";
+import MessageBoard from "../widgets/MessageBoard";
+import ClockWidget from "../widgets/Clock";
+import { useNavigation } from "@react-navigation/native";
 
 const DesignPage = ({ route }) => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const handleWeatherWidgetPress = () => {
-        console.log('Weather Widget Pressed');
-    };
+  const handleWeatherWidgetPress = () => {
+    console.log("Weather Widget Pressed");
+  };
 
-    return (
-        <ScrollView style={[]}>
-            <View style={styles.main}>
-                <Text style={styles.text}>Here we will show the design of different component</Text>
-                <Text style={styles.text}>Component that I am gonna design</Text>
-                <Text style={styles.text}>1. Nav Bar(Display when slide)</Text>
+  return (
+    <ScrollView style={[]}>
+      <View style={styles.main}>
+        <Text style={styles.text}>
+          Here we will show the design of different component
+        </Text>
+        <Text style={styles.text}>Component that I am gonna design</Text>
+        <Text style={styles.text}>1. Nav Bar(Display when slide)</Text>
 
-                <Text style={styles.text}>2. Message Board</Text>
-                <MessageBoard title="Example Heading" textList={["Example of paragraph.", "Another Example of Paragraph, which may occupy for two lines."]} />
-                <Text style={styles.text}>3. Weather Widget</Text>
-                {/* Testing for using TouchableOpacity for onPress() */}
-                <TouchableOpacity onPress={handleWeatherWidgetPress}>
-                    <WeatherWidget />
-                </TouchableOpacity>
-                <Text style={styles.text}>4. Clock Widget</Text>
-                <ClockWidget />
-            </View >
-        </ScrollView>
-
-    );
-}
-
+        <Text style={styles.text}>2. Message Board</Text>
+        <MessageBoard
+          title="Example Heading"
+          textList={[
+            "Example of paragraph.",
+            "Another Example of Paragraph, which may occupy for two lines.",
+          ]}
+        />
+        <Text style={styles.text}>3. Weather Widget</Text>
+        {/* Testing for using TouchableOpacity for onPress() */}
+        <TouchableOpacity onPress={handleWeatherWidgetPress}>
+          <WeatherWidget />
+        </TouchableOpacity>
+        <Text style={styles.text}>4. Clock Widget</Text>
+        <ClockWidget />
+      </View>
+    </ScrollView>
+  );
+};
 
 // other style
 const styles = StyleSheet.create({
-    main: {
-        backgroundColor: '#000000',
-        width: '100%',
-        height: 1000
-    },
-    text: {
-        color: '#00f0ff'
-    },
-    box: {
+  main: {
+    backgroundColor: "#000000",
+    width: "100%",
+    height: 1000,
+  },
+  text: {
+    color: "#00f0ff",
+  },
+  box: {
+    // Size
+    width: 100,
+    height: 100,
 
-        // Size
-        width: 100,
-        height: 100,
+    // outer
+    marginTop: 10,
 
-        // outer
-        marginTop: 10,
+    // inner
+    backgroundColor: "rgba(0, 240, 255, 0.05)",
+    padding: 10,
 
-        // inner
-        backgroundColor: 'rgba(0, 240, 255, 0.05)',
-        padding: 10,
+    // border
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#00f0ff",
 
-        // border
-        borderRadius: 10,
-        borderWidth: 2,
-        borderColor: '#00f0ff',
-
-        // shadow
-        shadowOffset: { height: 1, width: 1 },
-        shadowColor: '#00f0ff',
-        shadowRadius: 0.1,
-        shadowOpacity: 0.5,
-    },
+    // shadow
+    shadowOffset: { height: 1, width: 1 },
+    shadowColor: "#00f0ff",
+    shadowRadius: 0.1,
+    shadowOpacity: 0.5,
+  },
 });
 
 export default DesignPage;
