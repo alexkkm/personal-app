b14603165800ef0f3769732b6aaf77e8c917ecb3
alexkkm <alexkong0222@gmail.com>
2025-02-03 00:50:38 +0800

    added color.js for coloring

---
+++ utils/color.js
@@ -0,0 +1,11 @@
+const light = {
+    background: '#ffffff',
+    primary: '#140769',
+    secondary: '#232323',
+}
+
+const dark = {
+    background: '#000000',
+    primary: '#00f0ff',
+    secondary: 'rgba(0, 240, 255, 0.05)',
+}
